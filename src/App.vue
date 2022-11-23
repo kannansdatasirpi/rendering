b379dd259ade52463aa4968be704644f9692694d
kannan <kannans@datasirpi.com>
2022-11-23 07:48:57 +0530

    conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
+<!--conditional rendering have 4 directives v-if, v-else, v-else-if, v-show-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2 v-if="num === 0">The Number is Zero</h2> <!--conditionaly rendering element we use v-if directive-->
+  <h2 v-else-if="num < 0">The Number is Negative</h2> <!--conditionaly rendering element we use v-else-if directive-->
+  <h2 v-else-if="num > 0">The Number is Positive</h2> <!--conditionaly rendering element we use v-else=if directive-->
+  <h2 v-else>The Number is Not Zero</h2> <!--conditionaly rendering element we use v-else directive-->
+
+<!--conditional rendering multiple element -->
+  <template v-if="display">
+    <h2>abcd</h2>
+    <h2>efgh</h2>
+    <h2>ijkl</h2>
+  </template>
+
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+    name :'App',
+    data(){
+        return {
+            num: "hai",
+            display: true,
+        } 
+    }
 }
 </script>
+
 
 <style>
 #app {
